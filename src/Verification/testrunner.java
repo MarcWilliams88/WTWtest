081ad1c83aa712e3977586ed3bcf27ab26d6c4d8
MarcWilliams88 <79200715+MarcWilliams88@users.noreply.github.com>
2021-02-19 12:21:43 +0000

    Update testrunner.java
    test1

--- src/Verification/testrunner.java
+++ src/Verification/testrunner.java
@@ -5,18 +5,17 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features", glue={"Verification"},
+@CucumberOptions(features="test.feature", glue={"Verification"},
 monochrome = true,
 
 //REPORTING
 plugin = {"pretty", "html:target/HtmlReports", 
 "json:target/JSONReports/report.json", 
-"junit:target/JunitReports/report.xml"}
+"junit:target/JunitReports/report.xml"},
 
 //FILTERING BY TAG
-//tags="@ScenarioLogin"
+tags="@ScenarioBrowser"
 )
 public class testrunner {
 
 }
-

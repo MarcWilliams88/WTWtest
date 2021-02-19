e3b80bee7015f06c7df11426497160cb817cb473
MarcWilliams88 <79200715+MarcWilliams88@users.noreply.github.com>
2021-02-19 12:21:01 +0000

    Update Location.java
    test1

--- src/Verification/Location.java
+++ src/Verification/Location.java
@@ -1,44 +1,69 @@
 package Verification;
 
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class Location {
 	
-	static WebDriver driver = null;
+	WebDriver driver = null;
 	
+	@Given("Browser is open")
+	public void Browser_is_open() {
 	
-	 public static void main(String[] args){
-	      
-	System.setProperty("webdriver.chrome.driver", "C:/Users/marcw/Desktop/DEV/chromedriver_win32/chromedriver.exe");
-	driver = new ChromeDriver();
-	System.out.println("Browser is open");
-	driver.get("https://www.willistowerswatson.com/ICT");
+		String projectPath = System.getProperty("User.dir");
+        System.setProperty("webdriver.chrome.driver", projectPath + "DEV/chromedriver_win32/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+    	System.out.println("Browser is open");
+    	    	
+        driver.get("https:willistowerswatson.com/ict");
+	}
 	
-	//WebDriverWait wait = new WebDriverWait(driver, 40);
-	//wait.until(ExpectedConditions.numberOfWindowsToBe(1));
-			
-			//elementToBeClickable(By.id("pop-div03596980083915491")));
-
+	@And("User is on homepage")
+	public void User_is_on_homepage() {
+		
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        //cookie pop-up handle
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        driver.findElement(By.xpath("/html[1]/body[1]/div[4]")).click();
+       //Click on US/EN 
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html[1]/body[1]/nav[1]/section[1]/div[1]/button[1]/i[2]")).click();
+      //Select United States
+        driver.findElement(By.xpath("/html[1]/body[1]/nav[1]/section[2]/section[2]/div[1]/div[1]/div[1]/div[1]/div[1]/button[1]/i[1]")).click();
+       //Select English
+        driver.findElement(By.linkText("English")).click();    
+	}
 	
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	driver.findElement(By.xpath("/html[1]/body[1]/div[4]")).click();
-	//driver.findElement(By.xpath("/html[1]/body[1]/nav[1]/section[1]/div[1]/button[1]/i[2]")).click();
-	//driver.findElement(By.xpath("/html[1]/body[1]/nav[1]/section[2]/section[2]/div[1]/div[1]/div[1]/div[1]/div[1]/button[1]/i[1]")).click();
-	//driver.findElement(By.linkText("English")).click();
+	@When("Check sorting")
+	public void Check_sorting() {
+		 //Search
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//i[contains(text(),'search')]")).click();
+        driver.findElement(By.xpath("/html[1]/body[1]/nav[1]/section[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/form[1]/input[1]")).sendKeys("IFRS 17");
+        
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//span[@id='coveo9de96e90']")).click();
+	}
 	
+	@And("Filter")
+	public void Filter(){
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("div[1]/div[2]/div[1]/div[4]/div[1]/ul[1]/li[1]/label[1]/div[1]/div[1]")).click();
+	}
 	
-
-	
+	@Then("Validate Article")
+	public void Validate_Article() {
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		String confirmText = driver.findElement(By.xpath("1")).getText();
+		Assert.assertTrue("https://www.willistowerswatson.com/en-US/", confirmText.contains("en-US"));
 	}
 	}
-
